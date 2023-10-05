1cfcc43f8a557f180b91684a8cf413e750acff9d
Roman Makukha <Romanmakukha1989@gmail.com>
2023-10-06 01:45:12 +0300

    take_out

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,8 +16,5 @@
     </body>
 
         <script src="https://kinobox.tv/kinobox.min.js"></script>
-    <script src="https://unpkg.com/vue"></script>
-    <script src="https://unpkg.com/vueperslides"></script>
-    <link href="https://unpkg.com/vueperslides/dist/vueperslides.css" rel="stylesheet">
 
 </html>
