db8fb83bcf97823d2300824b3660d535a6e2ef05
Roman Makukha <Romanmakukha1989@gmail.com>
2023-10-06 02:02:50 +0300

    Title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
