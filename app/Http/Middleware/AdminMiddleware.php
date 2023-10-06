3a6c47b22380c0a570f980fb5f4756dd86cb8227
Roman Makukha <Romanmakukha1989@gmail.com>
2023-10-06 22:55:37 +0300

    log_out_added

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
@@ -16,6 +17,13 @@ class AdminMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if(auth()->user()->role != '1'){
+
+            Auth::guard('web')->logout();
+
+            $request->session()->invalidate();
+
+            $request->session()->regenerateToken();
+
             abort(403);
         }
 
