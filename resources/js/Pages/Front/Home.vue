faa2ee546ea92bc0d98d300e5d7a395457679103
myWebSteps <139718736+myWebSteps@users.noreply.github.com>
2024-01-31 15:37:44 +0300

    Collections_title_on_main_page

--- resources/js/Pages/Front/Home.vue
+++ resources/js/Pages/Front/Home.vue
@@ -23,7 +23,7 @@
         <section class="content">
             <div class="row">
                 <div class="d-flex w-100 align-items-center justify-content-between mt-4 mb-3">
-                    <h1 class="h5 mb-0 text-gray-900">Коллекции</h1>
+                    <h1 class="h5 mb-0 text-gray-900">Подборки</h1>
                     <Link :href="`/collections`" class="text-sm">
                         Перейти ко всем &nbsp<i class="fas fa-eye fa-sm"></i>
                     </Link>
