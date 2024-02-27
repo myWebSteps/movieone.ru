675b37aec3df33a25a66e410f4cd97e4e41bfba4
myWebSteps <139718736+myWebSteps@users.noreply.github.com>
2024-02-27 19:21:52 +0300

    Metrika_lazy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,23 +15,6 @@
 
         <script async src="https://kinobox.tv/kinobox.min.js"></script>
 
-    <!-- Yandex.Metrika counter -->
-{{--    <script type="text/javascript" >--}}
-{{--        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};--}}
-{{--            m[i].l=1*new Date();--}}
-{{--            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}--}}
-{{--            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})--}}
-{{--        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");--}}
-
-{{--        ym(94438576, "init", {--}}
-{{--            defer: true,--}}
-{{--            clickmap:true,--}}
-{{--            trackLinks:true,--}}
-{{--            accurateTrackBounce:true--}}
-{{--        });--}}
-
-{{--    </script>--}}
-
     <script type="text/javascript">
         ( function () {
             'use strict';
