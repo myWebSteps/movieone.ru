0d5f374d4e4d14acfdf3356597b5080a2ca87ae4
myWebSteps <139718736+myWebSteps@users.noreply.github.com>
2024-02-27 20:39:15 +0300

    etrica_fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,83 +13,81 @@
         @inertia
     </body>
 
-        <script defer src="https://kinobox.tv/kinobox.min.js"></script>
+        <script src="https://kinobox.tv/kinobox.min.js"></script>
 
-{{--    <script type="text/javascript">--}}
-{{--        ( function () {--}}
-{{--            'use strict';--}}
+    <script type="text/javascript">
+        window.addEventListener('load', (event)=>{
+            // Флаг, что Метрика уже загрузилась.
+            var loadedMetrica = false,
+                // Ваш идентификатор сайта в Яндекс.Метрика.
+                metricaId     = 94438576,
+                // Переменная для хранения таймера.
+                timerId;
 
-{{--            // Флаг, что Метрика уже загрузилась.--}}
-{{--            var loadedMetrica = false,--}}
-{{--                // Ваш идентификатор сайта в Яндекс.Метрика.--}}
-{{--                metricaId     = 94438576,--}}
-{{--                // Переменная для хранения таймера.--}}
-{{--                timerId;--}}
+            // Для бота Яндекса грузим Метрику сразу без "отложки",
+            // чтобы в панели Метрики были зелёные кружочки
+            // при проверке корректности установки счётчика.
+            if ( navigator.userAgent.indexOf( 'YandexMetrika' ) > -1 ) {
+                loadMetrica();
+            } else {
+                // Подключаем Метрику, если юзер начал скроллить.
+                window.addEventListener( 'scroll', loadMetrica, {passive: true} );
 
-{{--            // Для бота Яндекса грузим Метрику сразу без "отложки",--}}
-{{--            // чтобы в панели Метрики были зелёные кружочки--}}
-{{--            // при проверке корректности установки счётчика.--}}
-{{--            if ( navigator.userAgent.indexOf( 'YandexMetrika' ) > -1 ) {--}}
-{{--                loadMetrica();--}}
-{{--            } else {--}}
-{{--                // Подключаем Метрику, если юзер начал скроллить.--}}
-{{--                window.addEventListener( 'scroll', loadMetrica, {passive: true} );--}}
+                // Подключаем Метрику, если юзер коснулся экрана.
+                window.addEventListener( 'touchstart', loadMetrica );
 
-{{--                // Подключаем Метрику, если юзер коснулся экрана.--}}
-{{--                window.addEventListener( 'touchstart', loadMetrica );--}}
+                // Подключаем Метрику, если юзер дернул мышкой.
+                document.addEventListener( 'mouseenter', loadMetrica );
 
-{{--                // Подключаем Метрику, если юзер дернул мышкой.--}}
-{{--                document.addEventListener( 'mouseenter', loadMetrica );--}}
+                // Подключаем Метрику, если юзер кликнул мышкой.
+                document.addEventListener( 'click', loadMetrica );
 
-{{--                // Подключаем Метрику, если юзер кликнул мышкой.--}}
-{{--                document.addEventListener( 'click', loadMetrica );--}}
+                // Подключаем Метрику при полной загрузке DOM дерева,
+                // с "отложкой" в 1 секунду через setTimeout,
+                // если пользователь ничего вообще не делал (фоллбэк).
+                document.addEventListener( 'DOMContentLoaded', loadFallback );
+            }
+            function loadFallback() {
+                timerId = setTimeout( loadMetrica, 2000 );
+            }
 
-{{--                // Подключаем Метрику при полной загрузке DOM дерева,--}}
-{{--                // с "отложкой" в 1 секунду через setTimeout,--}}
-{{--                // если пользователь ничего вообще не делал (фоллбэк).--}}
-{{--                document.addEventListener( 'DOMContentLoaded', loadFallback );--}}
-{{--            }--}}
+            function loadMetrica( e ) {
 
-{{--            function loadFallback() {--}}
-{{--                timerId = setTimeout( loadMetrica, 2000 );--}}
-{{--            }--}}
+                // Пишем отладку в консоль браузера.
+                if ( e && e.type ) {
+                    console.log( e.type );
+                } else {
+                    console.log( 'DOMContentLoaded' );
+                }
 
-{{--            function loadMetrica( e ) {--}}
+                // Если флаг загрузки Метрики отмечен,
+                // то ничего более не делаем.
+                if ( loadedMetrica ) {
+                    return;
+                }
 
-{{--                // Пишем отладку в консоль браузера.--}}
-{{--                if ( e && e.type ) {--}}
-{{--                    console.log( e.type );--}}
-{{--                } else {--}}
-{{--                    console.log( 'DOMContentLoaded' );--}}
-{{--                }--}}
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+                ym( metricaId, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });
 
-{{--                // Если флаг загрузки Метрики отмечен,--}}
-{{--                // то ничего более не делаем.--}}
-{{--                if ( loadedMetrica ) {--}}
-{{--                    return;--}}
-{{--                }--}}
+                // Отмечаем флаг, что Метрика загрузилась,
+                // чтобы не загружать её повторно при других
+                // событиях пользователя и старте фоллбэка.
+                loadedMetrica = true;
 
-{{--                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");--}}
-{{--                ym( metricaId, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });--}}
+                // Очищаем таймер, чтобы избежать лишних утечек памяти.
+                clearTimeout( timerId );
 
-{{--                // Отмечаем флаг, что Метрика загрузилась,--}}
-{{--                // чтобы не загружать её повторно при других--}}
-{{--                // событиях пользователя и старте фоллбэка.--}}
-{{--                loadedMetrica = true;--}}
+                // Отключаем всех наших слушателей от всех событий,
+                // чтобы избежать утечек памяти.
+                window.removeEventListener( 'scroll', loadMetrica );
+                window.removeEventListener( 'touchstart', loadMetrica );
+                document.removeEventListener( 'mouseenter', loadMetrica );
+                document.removeEventListener( 'click', loadMetrica );
+                document.removeEventListener( 'DOMContentLoaded', loadFallback );
+            }
 
-{{--                // Очищаем таймер, чтобы избежать лишних утечек памяти.--}}
-{{--                clearTimeout( timerId );--}}
-
-{{--                // Отключаем всех наших слушателей от всех событий,--}}
-{{--                // чтобы избежать утечек памяти.--}}
-{{--                window.removeEventListener( 'scroll', loadMetrica );--}}
-{{--                window.removeEventListener( 'touchstart', loadMetrica );--}}
-{{--                document.removeEventListener( 'mouseenter', loadMetrica );--}}
-{{--                document.removeEventListener( 'click', loadMetrica );--}}
-{{--                document.removeEventListener( 'DOMContentLoaded', loadFallback );--}}
-{{--            }--}}
-{{--        } )()--}}
-{{--    </script>--}}
+            })
+    </script>
 
 
     <!-- /Yandex.Metrika counter -->
