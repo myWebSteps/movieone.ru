650676e518a7d2cd030057fb296ad6221b39310d
myWebSteps <139718736+myWebSteps@users.noreply.github.com>
2024-03-27 10:47:19 +0300

    ya_metrika_front_layout

--- resources/js/Layouts/FrontLayout.vue
+++ resources/js/Layouts/FrontLayout.vue
@@ -325,9 +325,25 @@ export default {
     mounted() {
         this.makePlaylist()
         this.makeBookmarks()
+        this.loadMetrica()
     },
 
     methods: {
+        loadMetrica(){
+
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                ym(94438576, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+            });
+
+        },
         commenceSearch() {
             if (this.searchKey.length >= 3) {
                 router.get(`/search?key=${this.searchKey}`)
