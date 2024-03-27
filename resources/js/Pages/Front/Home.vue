8b8745c2fcb867a279d07036419ceea6451b8b2f
myWebSteps <139718736+myWebSteps@users.noreply.github.com>
2024-03-27 13:17:49 +0300

    added_js_file

--- resources/js/Pages/Front/Home.vue
+++ resources/js/Pages/Front/Home.vue
@@ -7,29 +7,29 @@
         <meta name="keywords" content="Онлайн кинотеатр, смотреть фильмы онлайн, без регистрации"/>
     </Head>
     <!--cover-->
-<!--    <section class="grid items-top main-container-->
-<!--    lg:sticky-->
-<!--    lg:top-0-->
-<!--    h-fit-->
-<!--    max-h-[40vh]-->
-<!--    lg:max-h-[60vh]-->
-<!--    z-10 overflow-y-hidden"-->
-<!--    >-->
-<!--        <img v-if="coverImg !== null" class="w-full object-contain row-start-1 row-end-1 col-start-1 col-end-1" :src="coverImg" alt="...">-->
+    <section class="grid items-top main-container
+    lg:sticky
+    lg:top-0
+    h-fit
+    max-h-[40vh]
+    lg:max-h-[60vh]
+    z-10 overflow-y-hidden"
+    >
+        <img v-if="coverImg !== null" class="w-full object-contain row-start-1 row-end-1 col-start-1 col-end-1" :src="coverImg" alt="...">
 
-<!--        <span class="row-start-1 row-end-1 col-start-1 col-end-1-->
-<!--        lg:parallax-->
-<!--        pt-[6%]-->
-<!--        pl-[10%]-->
-<!--        text-white opacity-60 font-extrabold-->
-<!--        text-xl-->
-<!--        sm:text-2xl-->
-<!--        md:text-3xl">-->
-<!--            MovieOne.Ru <br>-->
-<!--            <span class="font-light">Онлайн-кинотеатр</span>-->
-<!--        </span>-->
+        <span class="row-start-1 row-end-1 col-start-1 col-end-1
+        lg:parallax
+        pt-[6%]
+        pl-[10%]
+        text-white opacity-60 font-extrabold
+        text-xl
+        sm:text-2xl
+        md:text-3xl">
+            MovieOne.Ru <br>
+            <span class="font-light">Онлайн-кинотеатр</span>
+        </span>
 
-<!--    </section>-->
+    </section>
 
     <!--Main Wrapper-->
     <section class="mb-4 z-20 bg-slate-100 relative">
