a0debf9b9db1e6a88853716ae5d745e37b3e951b
myWebSteps <139718736+myWebSteps@users.noreply.github.com>
2024-04-02 00:05:21 +0300

    display_swap

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="yandex-verification" content="04c3bf7c897cb0df" />
         <link rel="icon" href="/img/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20,100,0,0" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20,100,0,0&display=swap" />
         <!-- Scripts -->
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
