b52855db49dc6b69712a8e1bbe83426bfafb8adf
myWebSteps <139718736+myWebSteps@users.noreply.github.com>
2024-04-01 17:13:03 +0300

    sitemap_added

--- resources/views/Sitemap/collections.blade.php
+++ resources/views/Sitemap/collections.blade.php
@@ -1,11 +1,9 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    @foreach ($collections as $collection)
-        <url>
-            <loc>{{route('sitemap.movie_single')}}</loc>
-            <lastmod>{{ $collection->updated_at->tz('UTC')->toAtomString() }}</lastmod>
-            <changefreq>weekly</changefreq>
-            <priority>0.9</priority>
-        </url>
+
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    @foreach($collections as $collection)
+        <sitemap>
+            <loc>{{route('sitemap.movie_single', $collection->slug)}}</loc>
+        </sitemap>
     @endforeach
-</urlset>
+</sitemapindex>
--- resources/views/Sitemap/movies.blade.php
+++ resources/views/Sitemap/movies.blade.php
@@ -1,11 +1,9 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    @foreach ($movies as $movie)
-        <url>
-            <loc>{{route('sitemap.movie_single')}}</loc>
-            <lastmod>{{ $movie->updated_at->tz('UTC')->toAtomString() }}</lastmod>
-            <changefreq>weekly</changefreq>
-            <priority>0.9</priority>
-        </url>
+
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    @foreach($movies as $movie)
+        <sitemap>
+            <loc>{{route('sitemap.movie_single', $movie->slug)}}</loc>
+        </sitemap>
     @endforeach
-</urlset>
+</sitemapindex>
