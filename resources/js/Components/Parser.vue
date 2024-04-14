f001266f75814cf5cea865e51830e7bf5e2d6cbf
myWebSteps <139718736+myWebSteps@users.noreply.github.com>
2024-04-14 22:20:17 +0300

    metrica_fix

--- resources/js/Components/Parser.vue
+++ resources/js/Components/Parser.vue
@@ -11,9 +11,9 @@
                 "
          :class="show.parser ? 'fixed' : 'absolute top-[1rem]'"
     >
-        <i v-if="show.parser === false" class="icon-filter_alt cursor-pointer"></i>
+        <i v-if="show.parser === false" class="icon-filter_alt cursor-pointer text-2xl"></i>
 
-        <i v-if="show.parser === true" class="icon-close cursor-pointer"></i>
+        <i v-if="show.parser === true" class="icon-close cursor-pointer text-2xl"></i>
     </div>
 
     <div
