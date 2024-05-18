af04380dc357b5814706f03613c87ab2a9342d7f
myWebSteps <Romanmakukha1989@gmail.com>
2024-05-18 16:42:52 +0300

    preload

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,6 @@
         <link rel="preload" href="../img/cover.webp" as="image" type="image/webp">
         <link rel="preload" href="../img/cover_min.webp" as="image" type="image/webp">
         <link rel="preload" href="../img/logo-icon.webp" as="image" type="image/webp">
-        <link rel="preconnect" href="//mc.yandex.ru" crossorigin>
         <!-- Scripts -->
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
@@ -17,7 +16,7 @@
     <body class="bg-slate-100">
         @inertia
         <!-- Yandex.Metrika counter -->
-        <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "../assets/metrica.js", "ym"); ym(94438576, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/94438576" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "../assets/metrica.js", "ym"); ym(94438576, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true }); </script> <noscript>...</noscript>
         <!-- /Yandex.Metrika counter -->
     </body>
 
