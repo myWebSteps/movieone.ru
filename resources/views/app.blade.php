1e87ec1869164b93cda7fd389344d3daacd4a8e8
myWebSteps <Romanmakukha1989@gmail.com>
2024-05-18 16:05:22 +0300

    preload

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,7 @@
         <link rel="icon" href="../img/favicon.ico" type="image/x-icon">
         <link rel="preload" href="../img/cover.webp" as="image" type="image/webp">
         <link rel="preload" href="../img/cover_min.webp" as="image" type="image/webp">
+        <link rel="preload" href="../img/logo-icon.webp" as="image" type="image/webp">
         <!-- Scripts -->
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
