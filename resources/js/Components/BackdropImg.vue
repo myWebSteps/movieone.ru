3ad9f9974949dfe441711c5d2a2549af47f0eb87
myWebSteps <Romanmakukha1989@gmail.com>
2024-06-02 07:22:22 +0300

    width and height added on single movie page

--- resources/js/Components/BackdropImg.vue
+++ resources/js/Components/BackdropImg.vue
@@ -1,7 +1,7 @@
 <template>
 
     <img v-if="img != null" class="w-full object-contain row-start-1 row-end-1 col-start-1 col-end-1" :src="img"
-         :alt="'Фоновая картинка ' + alt">
+         :alt="'Фоновая картинка ' + alt" width="1200" height="450">
 
 </template>
 
