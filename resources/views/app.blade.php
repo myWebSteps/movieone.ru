ec1b0d21b8ed136cb5463efb382ab2189c2974da
myWebSteps <Romanmakukha1989@gmail.com>
2024-06-05 22:55:23 +0300

    player.js without defer and preloaded

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
         <link rel="icon" href="../img/favicon.ico" type="image/x-icon">
         <link rel="preload" href="../img/logo-icon.webp" as="image" type="image/webp">
         <link rel="preload" href="../assets/metrica.js" as="script" />
-        <link rel="preload" href="../assets/player.js" as="script" />
+        <script async src="../assets/player.js"></script>
         <!-- Scripts -->
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
@@ -19,7 +19,7 @@
         <!-- Yandex.Metrika counter -->
         <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "../assets/metrica.js", "ym"); ym(94438576, "init", { clickmap:false, trackLinks:false, accurateTrackBounce:false }); </script> <noscript>...</noscript>
         <!-- /Yandex.Metrika counter -->
-        <script src="../assets/player.js"></script>
+
     </body>
 
 </html>
