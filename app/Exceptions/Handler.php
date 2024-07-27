f0433c411d52e97c2efc8e6bc0d6e72d386734f3
SeobitGit <tech@seobit.ru>
2024-07-27 10:14:52 +0300

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -27,4 +27,12 @@ class Handler extends ExceptionHandler
             //
         });
     }
+    function render($request, Throwable $exception) {
+        if ($this->isHttpException($exception)) {
+            if ($exception->getStatusCode() == 404) {
+                return response()->view('errors.404', [], 404);
+            }
+        }
+        return parent::render($request, $exception);
+    }
 }
