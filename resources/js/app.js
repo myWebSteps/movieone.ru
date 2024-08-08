e951e678a140ffae5db74bf3aea07707e8ae0f0f
Roman Makukha <Romanmakukha1989@gmail.com>
2024-08-08 23:58:04 +0300

    Style.css fixed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 import '../css/output.css';
-// import '../css/stygit le.css';
+import '../css/style.css';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
